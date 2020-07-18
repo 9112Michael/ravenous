3081d1590b15466c544cf1063ad9d94b12ae760f
9112Michael <sitkabikemike@yahoo.com>
2020-07-18 16:47:50 -0700

    Create Yelp module and add search method

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,10 @@
-const apiKey='JLmkoDUHzLAJEXO3ZH4MySZUO1MMqW3blHVkvlOki-C9QZPbKQSfmcjpnoH83JT_aMx5HzrpX4zKWYGXcbzqp54xWiegSq-ibD-ZYiyturmR7UaiA6CBBNZUsHUTX3Yx';
+const { default: SearchBar } = require("../components/SearchBar/SearchBar");
+
+const apiKey ='JLmkoDUHzLAJEXO3ZH4MySZUO1MMqW3blHVkvlOki-C9QZPbKQSfmcjpnoH83JT_aMx5HzrpX4zKWYGXcbzqp54xWiegSq-ibD-ZYiyturmR7UaiA6CBBNZUsHUTX3Yx';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+    }
+};
+
