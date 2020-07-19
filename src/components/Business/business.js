1771ad4856b446395fe5afacff4e5cc2044a4ccc
9112Michael <sitkabikemike@yahoo.com>
2020-07-19 09:24:12 -0700

    Modify image source

--- src/components/Business/business.js
+++ src/components/Business/business.js
@@ -3,11 +3,11 @@ import './Business.css';
 
 class Business extends React.Component {
     render() {
-      const { business} = this.props;
+      const { business } = this.props;
         return (
             <div className="Business">
   <div className="image-container">
-    <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+    <img src={business.imageSrc} alt=''/>
   </div>
     <h2>{business.name}</h2>
   <div className="Business-information">
