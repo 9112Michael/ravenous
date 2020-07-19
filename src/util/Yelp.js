dfb4c1bb69348db3e0c83f428c9131ec2dbe9346
9112Michael <sitkabikemike@yahoo.com>
2020-07-19 09:23:09 -0700

    Remove destructuring and repair syntax

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,9 +1,8 @@
-const { default: SearchBar } = require("../components/SearchBar/SearchBar");
 
 const apiKey ='JLmkoDUHzLAJEXO3ZH4MySZUO1MMqW3blHVkvlOki-C9QZPbKQSfmcjpnoH83JT_aMx5HzrpX4zKWYGXcbzqp54xWiegSq-ibD-ZYiyturmR7UaiA6CBBNZUsHUTX3Yx';
 
 const Yelp = {
-    search(term, location, sortBy) {
+    searchYelp(term, location, sortBy) {
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
             headers: {
             Authorization: `Bearer ${apiKey}`,
